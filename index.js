2219320f51024a9a57c1bafacb21c10523e8f4b6
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2015-06-05 12:00:56 +0200

    Handle promises

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
-function expect(actual, prepend) {
+let isPromise = require('is-promise');
+
+function expect(actual) {
     return {
         to: function(test) {
             let res = test(actual);
 
-            if (res === undefined) return;
-
-            let err = new Error(res.msg);
-            err.expected = res.expected;
-            err.actual = res.actual;
-            err.showDiff = true;
-            throw err;
+            if (isPromise(res)) {
+                return res.then(throwIfError);
+            } else {
+                return throwIfError(res);
+            }
         }
     }
 }
 
-function assert(passed, opts = {}) {
-    if (passed === true) return;
+function throwIfError(res) {
+    if (res === undefined) return;
 
-    return opts;
+    let err = new Error(res.msg);
+    err.expected = res.expected;
+    err.actual = res.actual;
+    err.showDiff = true;
+    throw err;
+}
+
+function assert(passed, opts = {}) {
+    if (passed !== true) {
+        return opts;
+    }
 }
 
 function equal(expected) {
-    return function(actual) {
-        return assert(expected === actual, {
-            expected: expected,
-            actual: actual,
-            msg: `Expected ${output(actual)} to equal ${output(expected)}`
-        });
-    }
+    return actual => assert(expected === actual, {
+        expected: expected,
+        actual: actual,
+        msg: `Expected ${output(actual)} to equal ${output(expected)}`
+    });
+}
+
+function eventually(test) {
+    return promise => promise.then(test);
 }
 
 function output(obj) {
@@ -37,5 +49,5 @@ function output(obj) {
     return obj;
 }
 
-module.exports = { expect, equal };
+module.exports = { expect, equal, eventually };
 
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-let { expect, equal } = require('./');
+let Promise = require("bluebird");
+let { expect, equal, eventually } = require('./');
 
 describe("expected", function() {
 
@@ -8,6 +9,22 @@ describe("expected", function() {
 
     it("should fail - boolean", function() {
         expect(true).to(equal(false));
+    });
+
+    it("should fail - promise", function() {
+        let promise = new Promise(function(resolve, reject) {
+            resolve("promise fail");
+        });
+
+        return expect(promise).to(eventually(equal("promise")));
+    });
+
+    it("should run - promise", function() {
+        let promise = new Promise(function(resolve, reject) {
+            resolve("promise");
+        });
+
+        return expect(promise).to(eventually(equal("promise")));
     });
 
     it("should run", function() {
