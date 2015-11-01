8c36561ba896a4e1452da4848d1430be3a3d3b78
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2015-11-01 13:07:14 +0100

    Two spaces instead of four

--- index.js
+++ index.js
@@ -2,41 +2,41 @@ import isPromise from 'is-promise';
 import stringify from './stringify';
 
 function assert(result, msg, err, expected) {
-    if (result === true) return;
-    return { msg, expected };
+  if (result === true) return;
+  return { msg, expected };
 }
 
 assert.not = function assertNot(result, msg, err, expected) {
-    if (result === false) return;
-    return { msg: err, expected };
+  if (result === false) return;
+  return { msg: err, expected };
 }
 
 function expect(actual) {
-    return {
-        to: test => {
-            const res = test({ actual, assert, stringify });
+  return {
+    to: test => {
+      const res = test({ actual, assert, stringify });
 
-            if (isPromise(res)) {
-                return res.then(
-                    throwIfError,
-                    throwIfError
-                );
-            }
-            else {
-                throwIfError(res);
-            }
-        }
+      if (isPromise(res)) {
+        return res.then(
+          throwIfError,
+          throwIfError
+        );
+      }
+      else {
+        throwIfError(res);
+      }
     }
+  }
 }
 
 function throwIfError(res) {
-    if (res !== undefined) {
-        const err = new Error(res.msg);
-        err.expected = res.expected;
-        err.actual = res.actual;
-        err.showDiff = true;
-        throw err;
-    }
+  if (res !== undefined) {
+    const err = new Error(res.msg);
+    err.expected = res.expected;
+    err.actual = res.actual;
+    err.showDiff = true;
+    throw err;
+  }
 }
 
 export default expect;
--- test.js
+++ test.js
@@ -5,52 +5,50 @@ import { not, equal } from './assertions';
 
 describe('expected', () => {
 
-    describe('equal', () => {
-        it('succeeds when strings are equal', () => {
-            expect('test').to(equal('test'));
-        });
-
-        it('fails when strings are not equal', () => {
-            assert.throws(() => {
-                expect('test').to(equal('testing'));
-            });
-        });
-
-        it('with not succeeds when strings are not equal', () => {
-            expect('test').to(not(equal('testing')));
-        });
-
-        it('with not fails when strings are equal', () => {
-            assert.throws(() => {
-                expect('test').to(not(equal('test')));
-            });
-        });
-
-        it('succeeds when booleans are equal', () => {
-            expect(true).to(equal(true));
-        });
-
-        it('fails when booleans are not equal', () => {
-            assert.throws(() => {
-                expect(true).to(equal(false));
-            });
-        });
-
-        it('succeeds when same reference', () => {
-            const test = { name: 'kim' };
-            expect(test).to(equal(test));
-        });
-
-        it('fails when different references', () => {
-            const ref1 = { name: 'kim' };
-            const ref2 = { name: 'kim' };
-
-            assert.throws(() => {
-                expect(ref1).to(equal(ref2));
-            });
-        });
+  describe('equal', () => {
+    it('succeeds when strings are equal', () => {
+      expect('test').to(equal('test'));
     });
 
+    it('fails when strings are not equal', () => {
+      assert.throws(() => {
+        expect('test').to(equal('testing'));
+      });
+    });
+
+    it('with not succeeds when strings are not equal', () => {
+      expect('test').to(not(equal('testing')));
+    });
+
+    it('with not fails when strings are equal', () => {
+      assert.throws(() => {
+        expect('test').to(not(equal('test')));
+      });
+    });
+
+    it('succeeds when booleans are equal', () => {
+      expect(true).to(equal(true));
+    });
+
+    it('fails when booleans are not equal', () => {
+      assert.throws(() => {
+        expect(true).to(equal(false));
+      });
+    });
+
+    it('succeeds when same reference', () => {
+      const test = { name: 'kim' };
+      expect(test).to(equal(test));
+    });
+
+    it('fails when different references', () => {
+      const ref1 = { name: 'kim' };
+      const ref2 = { name: 'kim' };
+
+      assert.throws(() => {
+        expect(ref1).to(equal(ref2));
+      });
+    });
+  });
 
 });
-
