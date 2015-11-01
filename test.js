6928d8552c3cd3b6d42b14f8f15bd904447bd80d
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2015-11-01 21:44:16 +0100

    Add initial tests for core expect

--- test.js
+++ test.js
@@ -1,6 +1,45 @@
-import assert from 'assert';
+import test from 'assert';
 import expect from './';
 
 describe('expect-to', () => {
+
+  it('triggers callback', () => {
+    let count = 0;
+    expect('test').to(() => {
+      count++;
+    });
+    test.equal(count, 1);
+  });
+
+  it('injects actual, assert and stringify', () => {
+    expect('test').to(({ actual, assert, stringify }) => {
+      test.equal(actual, 'test');
+      test.equal(typeof assert, 'function');
+      test.equal(stringify('test'), '"test"');
+      test.equal(stringify([1,2,3]), '[\n  1\n  2\n  3\n]');
+    });
+  });
+
+  it('throws when assert is false', () => {
+    const assertion = ({ assert }) => {
+      return assert(false, 'failed message', 'not message');
+    };
+
+    test.throws(
+      () => expect('test').to(assertion),
+      (err) => err.message === 'failed message'
+    );
+  });
+
+  it('throws when not and assert is true', () => {
+    const assertion = ({ assert }) => {
+      return assert.not(true, 'failed message', 'not message');
+    };
+
+    test.throws(
+      () => expect('test').to(assertion),
+      (err) => err.message === 'not message'
+    );
+  });
 
 });
