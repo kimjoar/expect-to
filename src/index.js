bfe2202840786cb559c38160a7b6b4e41e445e97
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2015-11-04 09:23:52 +0100

    Lint using Standard

--- src/index.js
+++ src/index.js
@@ -1,45 +1,44 @@
-import isPromise from 'is-promise';
-import stringify from './stringify';
+import isPromise from 'is-promise'
+import stringify from './stringify'
 
-function assert(result, msg, err, expected) {
-  if (result === true) return;
-  return { msg, expected };
+function assert (result, msg, err, expected) {
+  if (result === true) return
+  return { msg, expected }
 }
 
-assert.not = function assertNot(result, msg, err, expected) {
-  if (result === false) return;
-  return { msg: err, expected };
+assert.not = function assertNot (result, msg, err, expected) {
+  if (result === false) return
+  return { msg: err, expected }
 }
 
-function expect(actual) {
+function expect (actual) {
   return {
     to: test => {
-      const res = test({ actual, assert, stringify });
-      const throwIf = throwIfError(actual);
+      const res = test({ actual, assert, stringify })
+      const throwIf = throwIfError(actual)
 
       if (isPromise(res)) {
         return res.then(
           throwIf,
           throwIf
-        );
-      }
-      else {
-        throwIf(res);
+        )
+      } else {
+        throwIf(res)
       }
     }
   }
 }
 
-function throwIfError(actual) {
-  return function(res) {
+function throwIfError (actual) {
+  return function (res) {
     if (res !== undefined) {
-      const err = new Error(res.msg);
-      err.expected = res.expected;
-      err.actual = actual;
-      err.showDiff = true;
-      throw err;
+      const err = new Error(res.msg)
+      err.expected = res.expected
+      err.actual = actual
+      err.showDiff = true
+      throw err
     }
   }
 }
 
-export default expect;
+export default expect
--- test.js
+++ test.js
@@ -1,45 +1,45 @@
-import test from 'assert';
-import expect from './src';
+/* eslint-env mocha */
+
+import test from 'assert'
+import expect from './src'
 
 describe('expect-to', () => {
-
   it('triggers callback', () => {
-    let count = 0;
+    let count = 0
     expect('test').to(() => {
-      count++;
-    });
-    test.equal(count, 1);
-  });
+      count++
+    })
+    test.equal(count, 1)
+  })
 
   it('injects actual, assert and stringify', () => {
     expect('test').to(({ actual, assert, stringify }) => {
-      test.equal(actual, 'test');
-      test.equal(typeof assert, 'function');
-      test.equal(stringify('test'), '"test"');
-      test.equal(stringify([1,2,3]), '[\n  1\n  2\n  3\n]');
-    });
-  });
+      test.equal(actual, 'test')
+      test.equal(typeof assert, 'function')
+      test.equal(stringify('test'), '"test"')
+      test.equal(stringify([1, 2, 3]), '[\n  1\n  2\n  3\n]')
+    })
+  })
 
   it('throws when assert is false', () => {
     const assertion = ({ assert }) => {
-      return assert(false, 'failed message', 'not message');
-    };
+      return assert(false, 'failed message', 'not message')
+    }
 
     test.throws(
       () => expect('test').to(assertion),
       (err) => err.message === 'failed message'
-    );
-  });
+    )
+  })
 
   it('throws when not and assert is true', () => {
     const assertion = ({ assert }) => {
-      return assert.not(true, 'failed message', 'not message');
-    };
+      return assert.not(true, 'failed message', 'not message')
+    }
 
     test.throws(
       () => expect('test').to(assertion),
       (err) => err.message === 'not message'
-    );
-  });
-
-});
+    )
+  })
+})
