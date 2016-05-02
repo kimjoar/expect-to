19618742b75752819d9904cf2e4599a1a5691834
spalger <email@spalger.com>
2016-05-01 19:51:26 -0700

    switch out formatMsg module with custom-util-format

--- src/msg.js
+++ src/msg.js
@@ -1,37 +1,10 @@
-import { inspect } from 'util';
+import stringify from './stringify'
+import customUtilFormat from 'custom-util-format'
 
-export const inspectOpts = {
-  hidden: false,
-  color: true,
-  depth: 10
-};
-
-export function formatAs(flag, val) {
-  switch (flag) {
-    case 'j':
-      return inspect(val, inspectOpts);
-
-    case 'n':
-      return parseFloat(val);
-
-    case 's':
-    default:
-      return String(val);
-  }
-}
+const format = customUtilFormat({
+  j: stringify
+})
 
 export function formatMsg(msg) {
-  if (!Array.isArray(msg)) return msg;
-
-  const format = msg[0] || '';
-  const args = msg.slice(1);
-
-  return [
-    format.replace(/(.?)%([jsd])/g, (a, pre, flag) => {
-      if (!args.length) return a;
-      if (pre === '%') return `%${flag}`;
-      return (pre || '') + formatAs(flag, args.shift());
-    }),
-    ...args
-  ].join(' ')
+  return Array.isArray(msg) ? format(...msg) : msg
 }
--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 /* eslint-env mocha */
 
 import test from 'assert'
+import sinon from 'sinon'
+
+import { formatMsg } from './src/msg'
+import * as msg from './src/msg'
 import expect, { equal, not } from './src'
+
+const stubs = []
 
 describe('expect-to', () => {
   it('triggers callback', () => {
@@ -85,5 +91,61 @@ describe('expect-to', () => {
   it('exports core methods', () => {
     expect('foo').to(equal('foo'))
     expect('foo').to(not(equal('bar')))
+  })
+
+  describe('assert msg', () => {
+    beforeEach(() => stubs.push(sinon.spy(msg, 'formatMsg')))
+    afterEach(() => stubs.splice(0).forEach((s) => s.restore()))
+
+    it('formats assertion messages only when necesarry', function () {
+      expect().to(({ assert }) => assert(true, ['msg %d', 1], 'msgNot', 1))
+      sinon.assert.notCalled(msg.formatMsg)
+
+      try {
+        expect().to(({ assert }) => assert(false, ['msg %d', 2], 'msgNot', 2))
+      } catch (err) {
+        expect(err.message).to(equal('msg 2'))
+      }
+
+      sinon.assert.calledOnce(msg.formatMsg)
+    })
+  })
+
+  describe('msg formatting', () => {
+    describe('%j placeholder', () => {
+      it('stringifies the arg', () => {
+        const arg = { baz: 1 }
+        const out = formatMsg(['foo %j bar', arg])
+        expect(out).to(equal(`foo {\n  "baz": 1\n} bar`))
+      })
+    })
+
+    describe('%s placeholder', () => {
+      it('stringifies with string constructor', () => {
+        expect(formatMsg(['%s %s', {}])).to(equal('[object Object] %s'))
+      })
+
+      it('stringifies with string constructor', () => {
+        expect(formatMsg(['-> %s', 'string'])).to(equal('-> string'))
+      })
+    })
+
+    describe('%d placeholder', () => {
+      it('formats the arg as a number', () => {
+        expect(formatMsg(['(%d)', {}])).to(equal('(NaN)'))
+      })
+
+      it('formats the arg as a number', () => {
+        expect(formatMsg(['** %d', Math.PI])).to(equal('** 3.141592653589793'))
+      })
+
+      it('formats the arg as a number', () => {
+        expect(formatMsg(['& %d &', 55])).to(equal('& 55 &'))
+      })
+
+      it('formats the arg as a number', () => {
+        expect(formatMsg(['%d', /not a number/])).to(equal('NaN'))
+      })
+    })
   })
 })
